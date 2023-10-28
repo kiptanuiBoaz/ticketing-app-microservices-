ab6d4c6051edf711b09141a5f82a1eeb9f458686
Kiptanui Boaz <boaserem022@gmail.com>
2023-10-28 09:13:04 +0300

    added custom error abstract class

--- auth/src/errors/Request-validation-error.ts
+++ auth/src/errors/Request-validation-error.ts
@@ -1,11 +1,24 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./Custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+    statusCode = 400;
     constructor(public errors: ValidationError[]) {
         super();
 
         //only because we are extending a built-in class
         Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+
+
+    serializeErrors() {
+        return this.errors.map(err => {
+            if (err.type === 'field') {
+                return { message: err.msg, field: err.path };
+            }
+            return { message: err.msg };
+
+        })
     }
 }
 
