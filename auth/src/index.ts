75087c7027b3ab3c84f66323dc89d105d502cace
Kiptanui Boaz <boaserem022@gmail.com>
2023-10-28 13:49:56 +0300

    added mongoDB cluster

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import "express-async-errors";
+import "express-async-errors";// dealing with async and error throwing issues
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -12,6 +13,7 @@ import { NotFoundedError } from "./errors/not-found-error";
 const app = express();
 app.use(json());
 
+//auth routes
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
@@ -22,8 +24,25 @@ app.all("*", async () => {
     throw new NotFoundedError();
 })
 
-app.use(errorHandler)
+//error hanling middleware
+app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("Auth server listening on port 3000!!");
-})
+
+const start = async () => {
+    try {
+        //db connection   
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    } catch (error: any) {
+        console.error(error.message);
+    }
+
+    //listent for traffic
+    app.listen(3000, () => {
+        console.log("Auth server listening on port 3000!!");
+    })
+
+}
+
+start();
+
+
