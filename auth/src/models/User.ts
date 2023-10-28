ebe9ef035828dc0dc227f80998e7b9c29cbc7e89
Kiptanui Boaz <boaserem022@gmail.com>
2023-10-28 16:54:16 +0300

    everything

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
+//an interface that describes the propterties required  to create a n ew user
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+//interface that describes the properties that the user model has
+interface UserModel extends mongoose.Model<any> {
+    build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -12,6 +23,11 @@ const userSchema = new mongoose.Schema({
 
 });
 
-const User = mongoose.model("User", userSchema);
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>("User", userSchema);
 
 export { User };
