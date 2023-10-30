6eb7af33948a19c6e9683153fd8c2c7837c12c74
Kiptanui Boaz <boaserem022@gmail.com>
2023-10-30 19:00:23 +0300

    added cookie session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";// dealing with async and error throwing issues
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -10,8 +11,16 @@ import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundedError } from "./errors/not-found-error";
 
+
 const app = express();
+//traffic is being proxied through ingress and nginx
+app.set("trust proxy", true);
 app.use(json());
+//cookie management middleware
+app.use(cookieSession({
+    signed: false,
+    secure: true,//musbe be http
+}))
 
 //auth routes
 app.use(currentUserRouter);
