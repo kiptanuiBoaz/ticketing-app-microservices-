4c730cfcf61326107411717b5392009b5ea6baa6
Kiptanui Boaz <boaserem022@gmail.com>
2023-11-25 09:28:06 +0300

    add current user andsignout tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -68,7 +68,7 @@ it("disallows duplicate email", async () => {
         .expect(400)
 });
 
-it("sets a cookie after successful signuup", async () => {
+it("sets a cookie after successful signup", async () => {
     const response = await request(app)
         .post("/api/users/signup")
         .send({
@@ -77,5 +77,6 @@ it("sets a cookie after successful signuup", async () => {
         })
         .expect(201);
 
+    //check headers
     expect(response.get("Set-Cookie")).toBeDefined();
 })
