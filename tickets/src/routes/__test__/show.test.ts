62038e314ec1ee70b064b40e861bf68a9fd91088
Kiptanui Boaz <boaserem022@gmail.com>
2024-01-28 11:39:23 +0300

    add tickets service

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,33 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+
+it("returns a 404 if the ticket is not found", async () => {
+    //generates a mongoode id
+    const id = new mongoose.Types.ObjectId().toHexString();
+
+    await request(app).get(`/api/tickets/${id}`).send().expect(404);
+});
+
+it("returns the ticket if the ticket is found", async () => {
+    const cookie = await global.signin();
+    const title = "concert";
+    const price = 20;
+
+    const response = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", cookie)
+        .send({
+            title: title,
+            price: price,
+        })
+        .expect(201);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+        .expect(200);
+
+    expect(ticketResponse.body.title).toEqual(title);
+    expect(ticketResponse.body.price).toEqual(price);
+});
---
+++ tickets/src/routes/update.ts
@@ -0,0 +1,62 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import {
+    validateRequest,
+    NotFoundError,
+    requireAuth,
+    NotAuthorizedError,
+    BadRequestError,
+} from "@ak-tickets-reuse/common";
+import { Ticket } from "../models/ticket";
+import { TicketUpdatedPublisher } from "./../events/publishers/ticket-updated";
+import { natsWrapper } from "../nats-wrapper";
+
+const router = express.Router();
+
+router.put(
+    "/api/tickets/:id",
+    requireAuth,
+    [
+        body("title").not().isEmpty().withMessage("Title is required"),
+        body("price")
+            .isFloat({ gt: 0 }) //gt: greater than
+            .withMessage("Proce must be provided and must be greater than 0"),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const ticket = await Ticket.findById(req.params.id);
+
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        if (ticket.orderId) {
+            throw new BadRequestError("Cannot edit a reserved ticket");
+        }
+
+        if (ticket.userID !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+
+        //update the document
+        ticket.set({
+            title: req.body.title,
+            price: req.body.price,
+        });
+
+        await ticket.save();
+
+        //publish the update event to NATS
+        new TicketUpdatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userID: ticket.userID,
+            version: ticket.version,
+        });
+
+        res.send(ticket);
+    }
+);
+
+export { router as updateTicketRouter };
