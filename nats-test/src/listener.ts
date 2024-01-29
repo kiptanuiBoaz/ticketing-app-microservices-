a6ea3d3dd437bc5970c8777f2c1355681b7dd482
Kiptanui Boaz <boaserem022@gmail.com>
2024-01-29 09:07:23 +0300

    add nats pub and nats pub

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,29 @@
+import nats from "node-nats-streaming";
+import { randomBytes } from "crypto";
+import { TicketCreatedListner } from "./events/ticket-created-listener";
+
+//clear the console
+console.clear();
+
+//initiate a nats client instance
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
+    url: "http://localhost:4222",
+});
+
+//run what is below when client is connected to the NATS streaming server
+stan.on("connect", () => {
+    console.log("Listener connected to NATS");
+
+    //run this code when a client receives an interupt
+    stan.on("close", () => {
+        console.log("NATS connection closed!");
+        process.exit();
+    });
+
+    //listen for events
+    new TicketCreatedListner(stan).listen();
+});
+
+//intercept interupt signals to our program
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,25 @@
+import nats from "node-nats-streaming";
+import { TiketCreatedPublisher } from "./events/ticket-created-publisher";
+
+//clear the console
+console.clear();
+
+//initiate a NATS client
+const stan = nats.connect("ticketing", "abc", {
+    url: "http://localhost:4222",
+});
+
+stan.on("connect", async () => {
+    console.log("Publisher connecteed to NATS");
+
+    const Publisher = new TiketCreatedPublisher(stan);
+    try {
+        await Publisher.publish({
+            id: "123",
+            title: "concerts",
+            price: 20,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
