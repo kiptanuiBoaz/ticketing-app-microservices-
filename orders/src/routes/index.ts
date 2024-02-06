683d65c52774f76214bc083e76fdef4e3e5a6efe
Kiptanui Boaz <boaserem022@gmail.com>
2024-02-06 11:03:49 +0300

    add orders service

---
+++ orders/src/routes/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import { requireAuth } from "@ak-tickets-reuse/common";
+import { Order } from "../models/order";
+
+const router = express.Router();
+
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
+    //shows all the orders belonging to a particular user
+    const orders = await Order.find({
+        userID: req.currentUser!.id,
+    }).populate("ticket");
+
+    res.send(orders);
+});
+
+export { router as indexOrderRouter };
