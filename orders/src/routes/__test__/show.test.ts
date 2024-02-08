80779b61e74142998f1fa1e6e277b0842c892799
Kiptanui Boaz <boaserem022@gmail.com>
2024-02-08 20:19:23 +0300

    add test to delete route

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,55 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import { app } from "../../app";
+import { Order, OrderStatus } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+it("fetches the order", async () => {
+    //create a ticket
+    const ticket = Ticket.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        title: "concert",
+        price: 30,
+    });
+    await ticket.save();
+
+    //make a request to build an order with the ticket
+    const user = await global.getAuthCookie();
+    const { body: order } = await request(app)
+        .post("/api/orders")
+        .set("Cookie", user)
+        .send({ ticketId: ticket.id })
+        .expect(201);
+
+    //make request to fetch the order
+    const { body: fetchedOrder } = await request(app)
+        .get(`/api/orders/${order.id}`)
+        .set("Cookie", user)
+        .expect(200);
+
+    // expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it("returns an error if one user tries to fetch another users order", async () => {
+    //create a ticket
+    const ticket = Ticket.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        title: "concert",
+        price: 30,
+    });
+    await ticket.save();
+
+    //make a request to build an order with the ticket
+    const { body: order } = await request(app)
+        .post("/api/orders")
+        .set("Cookie", await global.getAuthCookie())
+        .send({ ticketId: ticket.id })
+        .expect(201);
+
+    //make request to fetch the order
+    await request(app)
+        .get(`/api/orders/${order.id}`)
+        .set("Cookie", await global.getAuthCookie())
+        .send()
+        .expect(401);
+});
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,41 +1,41 @@
 import {
-    NotAuthorizedError,
-    NotFoundError,
-    requireAuth,
-    validateRequest,
-  } from "@ak-tickets-reuse/common";
-  // import { param } from "express-validator";
-  // import mongoose from "mongoose";
-  import express, { Request, Response } from "express";
-  import { Order } from "../models/order";
-  
-  const router = express.Router();
-  
-  // const isValidObjectId = (input: string) =>
-  //   mongoose.Types.ObjectId.isValid(input);
-  // const validateObjectId = param("")
-  //   .not()
-  //   .isEmpty()
-  //   .custom(isValidObjectId)
-  //   .withMessage("Order must be a valid ObjectId");
-  
-  router.get(
-    "/api/orders/:orderId",
-    requireAuth,
-    validateRequest,
-    async (req: Request, res: Response) => {
-      const order = await Order.findById(req.params.orderId);
-  
-      if (!order) {
-        throw new NotFoundError();
-      }
-  
-      if (order.userID !== req.currentUser!.id) {
-        throw new NotAuthorizedError();
-      }
-  
-      res.send(order);
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from "@gittixteam/common";
+// import { param } from "express-validator";
+// import mongoose from "mongoose";
+import express, { Request, Response } from "express";
+import { Order } from "../models/order";
+
+const router = express.Router();
+
+// const isValidObjectId = (input: string) =>
+//   mongoose.Types.ObjectId.isValid(input);
+// const validateObjectId = param("")
+//   .not()
+//   .isEmpty()
+//   .custom(isValidObjectId)
+//   .withMessage("Order must be a valid ObjectId");
+
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId);
+
+    if (!order) {
+      throw new NotFoundError();
     }
-  );
-  
-  export { router as showOrderRouter };
+
+    if (order.userID !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
+
+export { router as showOrderRouter };
