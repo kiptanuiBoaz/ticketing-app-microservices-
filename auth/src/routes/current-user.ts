527e7f70bfc4dd88269c342289fdaa53672ed2d7
Kiptanui Boaz <boaserem022@gmail.com>
2024-09-12 08:26:09 +0300

    fix: add MongoURI env to auth deployment

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,8 +3,8 @@ import { currentUser } from '@gittixteam/common';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser });
 });
 
 export { router as currentUserRouter };
